7ff086fdcbf5f244f80eab6ffbe71d4d0cf75c64
Yan <brunshte@gmail.com>
2024-01-09 17:08:56 -0800

    StormViewer init commit

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by YB on 1/9/24.
+
+// iOS user interface toolkit
+import UIKit
+
+// When you see a data type that starts with “UI”, it means it comes from UIKit.
+// UIViewController is Apple’s default screen type, which is empty and white until we change it.
+class ViewController: UIViewController {
+    // will be created when the ViewController screen is created, and exist for as long as the screen exists. It will be empty, because we haven’t actually filled it with anything, but at least it’s there ready for us to fill.
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // shared file manager object. data type that lets us work with the filesystem, and in our case we'll be using it to look for files.
+        let fm = FileManager.default
+        // set to the resource path of our app's bundle. bundle is a directory containing our compiled program and all our assets. So, this line says, "tell me where I can find all those images I added to my app."
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+
+
+}
+
