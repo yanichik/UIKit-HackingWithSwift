805ae7d3e645621a713bed051f6db6561d84ef93
Yan <brunshte@gmail.com>
2024-01-18 15:51:57 -0800

    Guess the Flag init, add flags w/ auto layout using cntl+drag method, add IB outlet, set btn img and border

---
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by YB on 1/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+//        button3.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor(red: 1, green: 0.6, blue: 0.3, alpha: 0.5).cgColor
+        button3.layer.borderWidth = 3
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    
+}
+
